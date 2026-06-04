783d89ca4d4a775baddc9b6c8d93806a71d65ac2
m1stermax1 <maxipompas@gmail.com>
2026-06-04 12:13:23 -0300

    integrando render

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // CARGA EXPLICITA DEL .ENV
-dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
-});
+// dotenv.config({
+//   path: path.resolve(__dirname, "../.env"),
+// });
+
+dotenv.config();
 
 const app = express();
 
